892e23399e46fd92efdb8b51e1e0beba40cf6ea7
301-05\User <boyboy1962@gmail.com>
2021-09-24 22:05:21 +0900

    금요일 수업
    일단 여기 마론달그램에서는 기본포트(80)번으로 정상작동을 확인하였다.
    
    ### 숙제로 마크업 하기 매우 중요 ###
    로그인화면
    회원가입 화면
    
    이거 않하면 따라가기 벅차니 꼭하자

--- src/main/java/com/marondalgram/MarondalgramApplication.java
+++ src/main/java/com/marondalgram/MarondalgramApplication.java
@@ -1,8 +1,11 @@
 package com.marondalgram;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})//임시로 DB를 사용하지 않는다.
 @SpringBootApplication
 public class MarondalgramApplication {
 
--- src/main/java/com/marondalgram/test/TestController.java
+++ src/main/java/com/marondalgram/test/TestController.java
@@ -1,5 +1,16 @@
 package com.marondalgram.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
 
+	@ResponseBody
+	@RequestMapping("/test1")
+	public String test () {
+		return "!!hello masterJ!!";
+	}
 }
+
